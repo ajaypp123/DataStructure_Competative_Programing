9edb24dd5c815a92c4cf0630dc81ff993f46d529
Ajay Pandurang Paratmandali <ajaypp123@gmail.com>
2022-05-01 10:57:35 +0530

    Time: 8 ms (8.96%), Space: 42.2 MB (45.95%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    private String getStr(String str) {
+        Stack<Character> st = new Stack<>();
+        String sb = "";
+        
+        for(char ch: str.toCharArray()) {
+            if(ch == '#' && !st.isEmpty()) {
+                st.pop();
+            } else if(ch == '#') {
+                continue;
+            } else {
+                st.add(ch);
+            }
+        }
+        
+        while(!st.isEmpty()) {
+            sb = st.pop() + sb;
+        }
+        System.out.println(sb);
+        return sb;
+    }
+    
+    public boolean backspaceCompare(String s, String t) {
+        return getStr(s).equals(getStr(t));
+    }
+    
+    
+}
