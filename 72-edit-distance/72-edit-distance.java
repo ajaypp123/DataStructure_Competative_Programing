56a54460ab5017fa2908f5437b8d7cd1527bec11
Ajay Pandurang Paratmandali <ajaypp123@gmail.com>
2022-05-01 14:46:11 +0530

    Time: 9 ms (41.29%), Space: 45.5 MB (11.14%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        /*
+            total = len of word1 - len of LCS
+        */
+        int m = word1.length();
+        int n = word2.length();
+        //create  dp table
+        int[][] dp = new int[m+1][n+1];
+        //base case 
+        for(int i = 0; i<= m;i++){
+            dp[i][0] = i;
+        }
+        for(int j = 1; j<=n; j++){
+            dp[0][j] = j;
+        }
+        //bottom-up
+        for(int i = 1; i<=m;i++){
+            for(int j = 1; j<=n;j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = min(dp[i-1][j]+1,dp[i][j-1]+1,dp[i-1][j-1]+1);
+                }
+            }
+        }
+        return dp[m][n];
+        
+    }
+    
+    int min(int a, int b,int c){
+        return Math.min(a,Math.min(b,c));
+    }
+}
